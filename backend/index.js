0b890ed9479443f368e3ea96966851083a649605
Adnan <21bec003@iiitdwd.ac.in>
2024-11-13 21:30:10 +0530

    Overall App

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
 app.use('/api/courses', courseRoutes)
 app.use('/api/user', userRoutes)
 
+
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     app.listen(port, () => {
